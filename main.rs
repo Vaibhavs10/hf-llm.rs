2c2dd2db51a88f03e7b377123210894249a98142
Vaibhav Srivastav <vaibhavs10@gmail.com>
2024-08-30 14:53:07 +0200

    Simple CLI

--- main.rs
+++ main.rs
@@ -1,3 +1,8 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter a string: ");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    println!("You entered: {}", input);
 }
