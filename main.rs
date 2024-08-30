f27379d79686b38b3767308ccc38180ac6852d3b
Vaibhav Srivastav <vaibhavs10@gmail.com>
2024-08-30 14:58:26 +0200

    Simple CLI w/ Two inputs.

--- main.rs
+++ main.rs
@@ -1,8 +1,13 @@
 use std::io;
 
 fn main() {
-    println!("Enter a string: ");
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read line");
-    println!("You entered: {}", input);
+    println!("Enter the first string: ");
+    let mut input1 = String::new();
+    io::stdin().read_line(&mut input1).expect("Failed to read line");
+    
+    println!("Enter the second string: ");
+    let mut input2 = String::new();
+    io::stdin().read_line(&mut input2).expect("Failed to read line");
+
+    println!("You entered: {} and {}", input1.trim(), input2.trim());
 }
